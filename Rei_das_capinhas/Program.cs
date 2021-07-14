c341a3e93287902775e4216514a45818b7c71d06
Eduardo CFN <eduardo@verion.ind.br>
2021-07-14 10:53:47 -0300

    Add project files.

---
+++ Rei_das_capinhas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rei_das_capinhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
