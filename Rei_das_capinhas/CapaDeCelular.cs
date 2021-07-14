7708324e414b8746f2e71c2ebcea82b3cf12b7aa
Eduardo CFN <eduardo@verion.ind.br>
2021-07-14 12:11:57 -0300

    *Criação da assinatura dos  métodos.

---
+++ Rei_das_capinhas/CapaDeCelular.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rei_das_capinhas
+{
+    class CapaDeCelular
+    {
+        public float Valor_unitario { get; set; }
+
+
+
+    }
+}
---
+++ Rei_das_capinhas/Loja.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rei_das_capinhas
+{
+    class Loja
+    {
+        ObservableCollection<CapaDeCelular> capas_compradas;
+        ObservableCollection<CapaDeCelular> capas_vendidas;
+
+        public Loja() 
+        {
+            capas_compradas = new ObservableCollection<CapaDeCelular>();
+            capas_vendidas = new ObservableCollection<CapaDeCelular>();
+
+            //TODO: Criar 3 handlers(Valor monetario das capas(compradas e vendidas) e do lucro)
+            //para a lista observada 'capas_compradas' que deverá ser exibido na tela.
+            //TODO: Criar 3 handlers(Valor monetario das capas(compradas e vendidas) e do lucro)
+            //para a lista observada 'capas_vendidas' que deverá ser exibido na tela.
+        }
+
+        public void comprar_CapaDeCelular(int quantidadeDoLote, float valor_unitario)
+        {
+            //TODO: comprar_CapaDeCelular
+        }
+
+        public void vender_CapaDeCelular(int quantidadeDoLote, float valor_unitario)
+        {
+            //TODO: vender_CapaDeCelular
+        }
+
+        public float calcular_valor_lista(ObservableCollection<CapaDeCelular> lista)
+        {
+            float valorTotalDaLista = 0;
+
+            //TODO: calcular_valor_lista
+
+            return valorTotalDaLista;
+        }
+
+        public float aferir_lucro() 
+        {
+            float valor_lucro = 0;
+
+            //TODO: aferir_lucro
+
+            return valor_lucro;
+        }
+
+
+    }
+}
